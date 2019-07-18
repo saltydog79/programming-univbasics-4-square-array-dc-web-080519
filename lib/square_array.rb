c4c0709819f0b644e6479335c4f2775be53673f1
Emily G <aspentree79@gmail.com>
2019-07-18 18:36:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
 counter = 0
-result = []
+array = []
 
   while counter < array.length do
-  puts result[counter] ** 2
+  puts array[counter] ** 2
     counter += 1
   end
 end
