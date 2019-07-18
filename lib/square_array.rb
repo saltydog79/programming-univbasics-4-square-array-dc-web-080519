2e6aeb2fef07d93abacb547b9bd53a51538d37c1
Emily G <aspentree79@gmail.com>
2019-07-18 19:16:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ counter = 0
 
   while counter < array.length do
     new_array = []
-    new array << array[counter] ** 2
+    new_array << array[counter] ** 2
     counter += 1
   end
   return new_array
